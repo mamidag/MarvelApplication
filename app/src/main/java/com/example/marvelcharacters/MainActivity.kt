ce86959bd6ff5f3cf7a71dfca62ad4926a9fc78e
Muhammed Dag <mamidag6@gmail.com>
2020-12-05 03:04:38 +0300

    init commit

--- app/src/main/java/com/example/marvelcharacters/MainActivity.kt
+++ app/src/main/java/com/example/marvelcharacters/MainActivity.kt
@@ -2,15 +2,10 @@ package com.example.marvelcharacters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.ObsoleteCoroutinesApi
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
